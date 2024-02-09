04c38c17cb077b57f11aee79dc860934a83e9718
Spiros Ioannou <sivann@gmail.com>
2024-02-09 11:34:25 +0200

    pex-mp

--- pex-mp.sh
+++ pex-mp.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+if [[ -z "$VIRTUAL_ENV" ]] ; then
+    echo "not running in virtualenv, exiting"
+    exit 1
+fi
+
 echo "Make sure all required python3's are in path"
+echo ""
 echo "Creating pex with for following platforms:"
 pex3 interpreter inspect --all --verbose --indent 4 |jq -r  .platform | egrep  'cp-3.[1,8,9][0-9]*'
+echo ""
 
 platforms=$(pex3 interpreter inspect --all --verbose --indent 4 |jq -r  .platform | egrep  'cp-3.[1,8,9][0-9]*'| sed -e 's/^/--platform /' | tr '\n' ' ')
 pex . --disable-cache -o kafka-lagstats -c kafka-lagstats.py --python-shebang '#!/usr/bin/env python3'
